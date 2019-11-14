8c080827f219ff9f820e77a823ab7bd8230b2d1c
ewellsgray <54516735+ewellsgray@users.noreply.github.com>
2019-11-13 22:01:56 -0500

    tightened up the distributions subplots

--- practical_stats/chapter1.py
+++ practical_stats/chapter1.py
@@ -8,34 +8,47 @@ import seaborn as sn
 #%% Simulate populations for testing
 # The aim here have a large enough sample point that it fully represents the 
 # population distribution and statistics
-
-def plot_dist(dist, color,new=True):
-    if new == True:
-        fig,(ax1,ax2) = plt.subplots(1,2,(figsize=(5,5))
-    
-    ax1.hist(dist,40,color=color, edgecolor='black')
-    #plt.hist(dist,40,color=color, edgecolor='black')
-    plt.xlabel("value")
-    plt.ylabel("count")    
-    return 0
-
+  
 # Population 1: normal distribution with mean = 10 and standard deviation = 2
 u1, sig1 = 10, 2
 n1 = 1000000
 norm1 = np.random.normal(u1,sig1,n1)
-plot_dist(norm1, "b")
 
 # Population 2: normal distribution with mean = 10 and standard deviation = 4
 u2, sig2 = 10, 4
 n2 = 1000000
 norm2 = np.random.normal(u2,sig2,n2)
-plot_dist(norm2,"r", new=False)
 
 # Population 3: a Gamma dist with shape(k) = 2, scale(t) = 1
 k3, t3 = 2, 3
 n3 = 1000000
 gamma3 = np.random.gamma(k3,t3,n3)
-plot_dist(gamma3,"g", new=True)
+
+
+fig,axes = plt.subplots(2,3,figsize=(10,5),sharex=True,squeeze=True)
+axes[0,0].hist(norm1,20,color='blue', edgecolor='black')
+axes[0,1].hist(norm2,20,color='red', edgecolor='black')
+axes[0,2].hist(gamma3,20,color='green', edgecolor='black')
+
+plt.sca(axes[1,0])
+sn.distplot(norm1,bins=20)
+plt.sca(axes[1,1])
+sn.distplot(norm2,bins=20)
+plt.sca(axes[1,2])
+sn.distplot(gamma3,bins=20)
+#plt.hist(dist,40,color=color, edgecolor='black')
+
+# Annotate the top row
+axes[0,0].set(xlabel="value", ylabel="count",title="Distribution 1")
+axes[0,1].set(xlabel="value", title="Distribution 2")
+axes[0,2].set(xlabel="value", title="Distribution 3")
+
+# Annotate the bottom row
+axes[1,0].set(xlabel="value", ylabel="prob",title="Probability Density")
+axes[1,1].set(xlabel="value", title="Probability Density")
+axes[1,2].set(xlabel="value", title="Probability Density")
+
+
 
 #%%
 n=20
