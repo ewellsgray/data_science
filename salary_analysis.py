2eead1e6bd7a625c49f569efdd2a1e5c742a32d3
ewellsgray <54516735+ewellsgray@users.noreply.github.com>
2020-03-01 13:45:23 -0500

    further EDA and some feature engineering

--- salary_analysis.py
+++ salary_analysis.py
@@ -10,6 +10,7 @@ import scipy.stats as stats
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+import seaborn as sn
 
 #%%
 path = 'C:\\Users\\ewell\\Documents\\CV\\nationwide data scientist specialist'
@@ -61,7 +62,9 @@ print("p val: "+str(round(p_val,4)))
 
 #%%
 # -- PREPARE THE DATA
-#%% binarize catagorical data
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OrdinalEncoder
+#%% binarize catagorical data and ordinal encode the title
 
 df.loc[df.discipline=="A","discipline"] = 0
 df.loc[df.discipline=="B","discipline"] = 1
@@ -69,46 +72,280 @@ df.loc[df.discipline=="B","discipline"] = 1
 df.loc[df.sex=="Male","sex"] = 0
 df.loc[df.sex=="Female","sex"] = 1
 
-#%% split x and y
-x = df.iloc[:,0:-1]
-y = df.iloc[:,-1]
-
-#%% Model --Linear Regression, no scaling
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OrdinalEncoder
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import make_pipeline
-from sklearn import metrics
-log_class = LogisticRegression()
-
+df.loc[df.title=="AssocProf","title"] = 1
+df.loc[df.title=="AsstProf","title"] = 0
+df.loc[df.title=="Prof","title"] = 2
 #%%
 def encode_column(encoding_type, col_name):
     
     if encoding_type=='label':
         le=LabelEncoder()
-        le.fit(x[col_name])
+        le.fit(df[col_name])
         title_order = list(le.classes_)
-        x[col_name] = le.fit_transform(x[col_name])
+        df[col_name] = le.fit_transform(df[col_name])
         print("Label Encoded")
     if encoding_type=='ordinal':
         oe=OrdinalEncoder()
-        oe.fit(x[col_name])
-        title_order = list(oe.classes_)
-        x[col_name] = oe.fit_transform(x[col_name])
+        Ord = [["AssocProf",1],["AsstProf",0],["Prof",2]]
+        oe.fit(Ord)
+        title_order = Ord
+        oe.transform(df[col_name])
         print("Ordinal Encoded")
 
     return
 #%%
 #encoding_type = 'label'
-encode_column("label","title")
+#encode_column("ordinal","title")
+
+#%%
+    
+#sn.scatterplot("years_phd","salary", hue="sex",data=df)
+#plt.show
+#sn.scatterplot("years_phd","salary", hue="discipline",data=df)
+#sn.scatterplot("years_phd","salary", hue="title",data=df)
+sn.scatterplot("years_phd","salary", hue="title",data=df)
+#%%
+sn.scatterplot("title","salary", hue="sex",data=df)
+
+#%%
+# -- EXPLORE THE DATA ------------------------------------
+#df.salary.hist()
+from pandas.plotting import scatter_matrix
+scatter_matrix(df, figsize=(12,9))
+#%%
+def plot_corr_matrix(df, subset=[], details="All Features"):
+    """Plots a correlation matrix of all the features. Can specify subset of features if desired"""
+
+    if len(subset)>0:
+        df = df[subset]
+        
+    corr = df.corr()
+    #print("First 3 correlations: "+str(corr.iloc[0,0:3]))
+    #cmap = sn.diverging_palette(255, 133, l=60, n=7, center="dark")
+    
+    # The values number here ar just for "nice" scaling
+    xx=(df.shape[1]/3,(df.shape[1]/3)*.85 )
+    fig=plt.figure(figsize=xx)
+    ax = sn.heatmap(corr,cmap='RdBu_r',vmin=-1,vmax=1)
+
+    ax.set_title("Correlation for "+details)
+#%%
+plot_corr_matrix(df)
+print("Corr Coef between years_phd and years_service: "+str(round(df.corr().iloc[2,3],2)))
+print("Corr Coef between title and TARGET: "+str(round(df.corr().iloc[0,5],2)))
+print("Corr Coef between discipline and TARGET: "+str(round(df.corr().iloc[1,5],2)))
+print("Corr Coef between years_phd and TARGET: "+str(round(df.corr().iloc[2,5],2)))
+print("Corr Coef between years_service and TARGET: "+str(round(df.corr().iloc[3,5],2)))
+print("Corr Coef between sex and TARGET: "+str(round(df.corr().iloc[4,5],2)))
+#%%
+df.groupby(["sex"]).mean()
+df.groupby(["title"]).mean()
+
+#%%
+#  ---- FEATURE ENGINEERIGN
+
+#%%
+#  1. Catagory for years_phd >45 and  25<years_phd<=45
+df["over_40"] = (df.years_phd>40)*1
+
+# 2. 
+df["year_diff"] = df.years_phd - df.years_service
+#%%
+def move_target_to_end(df,target_label):
+    
+    """Moves the target colums to the end of the DataFrame for easier book-keeping
+    and correlation matrix display"""
+    
+    cols = df.columns.tolist()
+    
+    p = cols.index(target_label)
+    cols_new=cols[0:p]
+    cols_new = cols_new + cols[p+1:]
+    cols_new.append(cols[p])
+    
+    df=df[cols_new]
+    return df
+
+#%%
+df = move_target_to_end(df,"salary")
+scatter_matrix(df, figsize=(12,9))
+plot_corr_matrix(df)
+
+#%%
+# --- CREATE TEST SET -----------------------------------------
+
+#%%
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+
+#x, x_test, y, y_test = train_test_split(x,y,test_size=0.2, random_state=0)
+
+#%%
+bins=np.array([50,80,100,120,160,250])*1000
+df["salary_cat"] = pd.cut(df["salary"], bins=bins,labels=[1,2,3,4,5])
+df.salary_cat.hist()
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=15)
+for train_index, test_index in split.split(df,df["salary_cat"]):
+    strat_train_set = df.loc[train_index]
+    strat_test_set = df.loc[test_index]
+
+"""    
+strat_test_set["salary_cat"].value_counts() / len(strat_test_set)
+
+def cat_proportions(data):
+    return data["salary_cat"].value_counts() / len(data)
+
+# ------------------------------------------------------------------------
+train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+# ------------------------------------------------------------------------
+compare_props = pd.DataFrame({
+    "Overall": cat_proportions(df),
+    "Stratified": cat_proportions(strat_test_set),
+    "Random": cat_proportions(test_set),
+}).sort_index()
+compare_props["Rand. %error"] = 100 * compare_props["Random"] / compare_props["Overall"] - 100
+compare_props["Strat. %error"] = 100 * compare_props["Stratified"] / compare_props["Overall"] - 100
+compare_props"""
+#%%
+"""def cat_proportions_sex(data):
+    return data["sex"].value_counts() / len(data)
+
+compare_props_sex = pd.DataFrame({
+    "Overall": cat_proportions_sex(df),
+    "Stratified": cat_proportions_sex(strat_test_set),
+    "Random": cat_proportions_sex(test_set),
+}).sort_index()
+compare_props_sex["Rand. %error"] = 100 * compare_props_sex["Random"] / compare_props_sex["Overall"] - 100
+compare_props_sex["Strat. %error"] = 100 * compare_props_sex["Stratified"] / compare_props_sex["Overall"] - 100
+compare_props_sex"""
+#%% split x and y
+
+strat_train_set.drop(["salary_cat"],axis=1, inplace=True)
+strat_test_set.drop(["salary_cat"],axis=1, inplace=True)
+df.drop(["salary_cat"],axis=1, inplace=True)
+
+x = strat_train_set.iloc[:,0:-1]
+y = strat_train_set.iloc[:,-1]
+x_o=x.copy()
+y_o=y.copy()
+
+#%% Model --Linear Regression, no scaling
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import power_transform
+from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import LinearSVR
+from sklearn.svm import NuSVR
+from sklearn.svm import SVR
+from sklearn import metrics
+from sklearn.decomposition import PCA
+log_class = LogisticRegression()
+
+
 #%%
 scalar = StandardScaler()
 xc = scalar.fit_transform(x)
+
+
+
+
+
+
+
+
+
+
 #%%
+
+def linear_reg_simple(X,Y,stsc=False, do_pca=False, boxcox=False):
+    X_train, X_val, Y_train, Y_val = train_test_split(X,Y,test_size=0.2, random_state=42)
+    
+    # Make the PipeLine list
+    S=[]
+    S.append(("lin_reg",LinearRegression()))
+    print("Linear Regression")
+    #bc = PowerTransformer(method='box-cox')
+    if do_pca==True:
+        S.insert(0,("pca",PCA(n_components=4)))
+        print("Using PCA")
+    if stsc==True:
+        S.insert(0,("stsc",StandardScaler()))
+        #model =make_pipeline(StandardScaler(),LinearRegression())
+        print("Using StandardScaler")
+        #print(X.iloc[5,:])
+
+
+    model = Pipeline(S)
+    model.fit(X_train,Y_train).score(X_train,Y_train)
+    Y_pred = model.predict(X_val)
+    
+    #print("Training MAE: {:.2}" .format(metrics.mean_absolute_error(model.predict(X_train),Y_train)))
+    #print("Validation MAE: {:.2}" .format(metrics.mean_absolute_error(Y_val,Y_pred)))
+    print(metrics.mean_absolute_error(model.predict(X_train),Y_train))
+    print(metrics.mean_absolute_error(Y_val,Y_pred))
+    print("Training R2: {:.2}" .format(metrics.r2_score(model.predict(X_train),Y_train)))
+    print("Validation R2: {:.2}" .format(metrics.r2_score(Y_val,Y_pred)))
+    print("\n")
+#%%
+def dec_tree_simple(X,Y,stsc=False, boxcox=False):
+    X_train, X_val, Y_train, Y_val = train_test_split(X,Y,test_size=0.2, random_state=42)
+    
+    if stsc==True:
+        model =make_pipeline(StandardScaler(),DecisionTreeRegressor())
+        print("Using StandardScaler")
+        #print(X.iloc[5,:])
+    else:
+        model =make_pipeline(DecisionTreeRegressor(max_depth=5,random_state=42))
+        print("Not Using StandardScaler")
+        #print(X.iloc[5,:])
+    #model = LinearRegression()
+    model.fit(X_train,Y_train).score(X_train,Y_train)
+    Y_pred = model.predict(X_val)
+    
+    print("Training MAE: {:.2}" .format(metrics.mean_absolute_error(model.predict(X_train),Y_train)))
+    print("Validation MAE: {:.2}" .format(metrics.mean_absolute_error(Y_val,Y_pred)))
+    #print(metrics.mean_absolute_error(model.predict(X_train),Y_train))
+    #print(metrics.mean_absolute_error(Y_val,Y_pred))
+    print("Training R2: {:.2}" .format(metrics.r2_score(model.predict(X_train),Y_train)))
+    print("Validation R2: {:.2}" .format(metrics.r2_score(Y_val,Y_pred)))
+    print("\n")
+#%%
+def svm_simple(X,Y,stsc=False):
+    X_train, X_val, Y_train, Y_val = train_test_split(X,Y,test_size=0.2, random_state=42)
+    
+    S=[]
+    S.append(("lin_SVR",LinearSVR()))
+    if stsc==True:
+        S.insert(0,("stsc",StandardScaler()))
+        #model =make_pipeline(StandardScaler(),NuSVR())
+        print("Using StandardScaler")
+        #print(X.iloc[5,:])
+    else:
+        model =make_pipeline(NuSVR())
+        print("Not Using StandardScaler")
+        #print(X.iloc[5,:])
+    #model = LinearRegression()
+    model.fit(X_train,Y_train).score(X_train,Y_train)
+    Y_pred = model.predict(X_val)
+    
+    print("Training MAE: {:.2}" .format(metrics.mean_absolute_error(model.predict(X_train),Y_train)))
+    print("Validation MAE: {:.2}" .format(metrics.mean_absolute_error(Y_val,Y_pred)))
+    #print(metrics.mean_absolute_error(model.predict(X_train),Y_train))
+    #print(metrics.mean_absolute_error(Y_val,Y_pred))
+    print("Training R2: {:.2}" .format(metrics.r2_score(model.predict(X_train),Y_train)))
+    print("Validation R2: {:.2}" .format(metrics.r2_score(Y_val,Y_pred)))
+    print("\n")
+    
+#%%
+linear_reg_simple(x,y,stsc=False)
+linear_reg_simple(x,y,do_pca=True, stsc=True)
+#dec_tree_simple(x,y,stsc=False)
+#svm_simple(x,y,stsc=True)
 
 #%%
 xc_train, xc_val, yc_train, yc_val = train_test_split(xc,y,test_size=0.2, random_state=0)
